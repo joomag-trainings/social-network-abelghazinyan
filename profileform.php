727858693f9ef338a4e56609790dfb8538a83f21
Abel <ghazinyan.abel@gmail.com>
2017-07-12 19:11:53 +0400

    Fixed upload problem

--- profileform.php
+++ profileform.php
@@ -1,4 +1,63 @@
-<!DOCTYPE html>
+<?php
+
+    $imageError = '';
+    if ($_SERVER['REQUEST_METHOD']==='POST') {
+        if ($_POST['form'] === 'profile') {
+            echo "<pre>";
+            var_dump($_FILES);
+            echo "</pre>";
+            $target_dir = "/var/www/html/social-network/Profile/";
+            $target_file = $target_dir . basename($_FILES["file"]["name"]);
+            echo $target_file;
+            $uploadOk = 1;
+            $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+            if(isset($_POST["submit"])) {
+                $check = getimagesize($_FILES["file"]["tmp_name"]);
+                if($check !== false) {
+                    $uploadOk = 1;
+                } else {
+                    $imageError = "File is not an image.";
+                    $uploadOk = 0;
+                }
+            }
+
+            if (file_exists($target_file)) {
+                $uploadOk = 0;
+            }
+
+            if ($_FILES["file"]["size"] > 500000) {
+                $imageError = "Sorry, your file is too large.";
+                $uploadOk = 0;
+            }
+
+            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                && $imageFileType != "gif" ) {
+                $imageError = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                $uploadOk = 0;
+            }
+            rename("/var/www/html/social-network/Profile/profile.jpg","/var/www/html/social-network/Profile/profileOld.jpg");
+
+            if ($uploadOk == 0) {
+                $imageError = "Sorry, your file was not uploaded.";
+
+            } else {
+              
+                if (!move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+                    rename("/var/www/html/social-network/Profile/profile.jpg","/var/www/html/social-network/Profile/profile.jpg");
+                    $imageError = "Sorry, there was an error uploading your file.";
+                }else {
+                    $name = "Profile/" . basename($_FILES["file"]["name"]);
+                    rename($name,"/var/www/html/social-network/Profile/profile.jpg");
+                }
+
+            }
+        }
+    }
+
+
+
+    ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -14,11 +73,17 @@
 <div class="body">
 
     <div class="profile">
-        <img src="Assets/profile.jpg">
+        <img src="Profile/profile.jpg">
     </div>
-    <button>Edit Picture</button>
     <div class="edit">
-    <form><br>
+        <form method="post" enctype="multipart/form-data">
+            <input type="hidden" name="form" value="profile">
+            <input type="file" name="file" id="file">
+            <input type="submit" value="Upload">
+        </form>
+        <span style="color:crimson;font-size: 15px;"><?php echo $imageError;?></span>
+    <form method="post"><br><br><br><br><br>
+        <input type="hidden" name="form" value="form">
         <label for="fname">First Name</label> <input type="text" name="fname" id="fname" value="Abel"><br><br>
         <label for="lname">Last  Name</label><input type="text" name="lname" id="lname" value="Ghazinyan"><br><br>
         <label for="email">E-Mail</label><input type="text"  id="email" name="email" placeholder="E-Mail" value="ghazinyan.abel@gmail.com"><br><br>
